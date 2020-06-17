75cf972f257e39093ea4504faeec2ea6e11494f4
FAVOUR <truelyfavoured@gmail.com>
2020-06-17 20:20:08 +0100

    Successfully fixed the bug experienced in the  ActivityFragmentSuit.kt class

--- app/src/androidTest/java/com/example/expresso_ui_test_example/suit/ActivityFragmentSuit.kt
+++ app/src/androidTest/java/com/example/expresso_ui_test_example/suit/ActivityFragmentSuit.kt
@@ -3,6 +3,7 @@ package com.example.expresso_ui_test_example.suit
 import com.example.expresso_ui_test_example.ui.movie.MainActivityTest
 import com.example.expresso_ui_test_example.ui.movie.MovieDetailFragmentTest
 import com.example.expresso_ui_test_example.ui.movie.StarActorsFragment
+import com.example.expresso_ui_test_example.ui.movie.StarActorsFragmentTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
@@ -10,9 +11,9 @@ import org.junit.runners.Suite
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
 
-//    MainActivityTest::class,
-//    MovieDetailFragmentTest::class,
-    StarActorsFragment::class
+    MainActivityTest::class,
+    MovieDetailFragmentTest::class,
+    StarActorsFragmentTest::class
 
 )
 class ActivityFragmentSuit {
