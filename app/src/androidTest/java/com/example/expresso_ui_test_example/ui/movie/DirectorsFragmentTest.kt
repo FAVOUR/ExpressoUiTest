753a6d20ef11953f23e78be4820d5adea0ba0f50
FAVOUR <truelyfavoured@gmail.com>
2020-06-17 20:35:10 +0100

    Successfully wrote a test for DirectorsFragmentTest.kt class and made some upgrade to StarActorsFragmentTest.kt

--- app/src/androidTest/java/com/example/expresso_ui_test_example/ui/movie/DirectorsFragmentTest.kt
+++ app/src/androidTest/java/com/example/expresso_ui_test_example/ui/movie/DirectorsFragmentTest.kt
@@ -11,43 +11,41 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.example.expresso_ui_test_example.R
 import com.example.expresso_ui_test_example.data.DummyMovies
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
-@RunWith(AndroidJUnit4ClassRunner::class)
-class StarActorsFragmentTest{
+@RunWith(AndroidJUnit4::class)
+class DirectorsFragmentTest{
 
     @Test
-    fun setupFragment_insertValues_checkIfStarActorDisplayed(){
+    fun setupFragment_insertValues_checkIfDirectorsAreDisplayed(){
         var fragmentFactory = FragmentFactory()
         var movie = DummyMovies.INFINITY_WAR
 
         var bundle = Bundle()
 
 //        bundle.putStringArrayList("args_actors", movie.star_actors)
-        bundle.putStringArrayList("args_actors", movie.star_actors)
+        bundle.putStringArrayList("args_directors", movie.directors)
 
         //var activitySenerio = launchFragmentInContainer<MovieDetailFragment> (fragmentArgs = bundle,factory = fragmentFactory ) //with factory
 
-        var fragmentScenario = launchFragmentInContainer<StarActorsFragment>(
-            fragmentArgs = bundle,
-            themeResId = R.style.AppTheme)   //Without the factory
+            var fragmentScenario = launchFragmentInContainer<DirectorsFragment>(
+            fragmentArgs = bundle,factory =fragmentFactory)   //Without the Style so it is either you pass the apps central theme sa style or you lunch the fragment
 
 //        delay(5000)
 
-        onView(ViewMatchers.withId(R.id.star_actors_text))
-            .check(matches(isDisplayed()))
-//        onView(ViewMatchers.withId(R.id.movie_description)).check(matches(withText("")))
+        onView(ViewMatchers.withId(R.id.directors_text))
+            .check(ViewAssertions.matches(isDisplayed()))
+        onView(ViewMatchers.withId(R.id.directors_text)).check(matches(withText(movie.directors?.let {
+            DirectorsFragment.stringBuilderForDirectors(
+                it
+            )
+        })))
 
 
     }
-
-
-
 
 }
